52b5360ad651593287050b40230cda5b43710264
mathengem <mathengem@aol.com>
2025-02-18 01:48:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
-   setup(
-       name='ffGEMM',
-       version='0.1.0',  # Replace with your desired version
-       packages=find_packages(),  # Automatically find packages within the repository
-       install_requires=[
-           # List any dependencies your package requires
-       ],
-   )
+setup(
+    name='ffGEMM',
+    version='0.1.0', 
+    packages=find_packages(),
+    install_requires=[
+        'gmpy2',  # Add gmpy2 as a dependency
+        'safetensors',  # Add safetensors as a dependency
+        # Add any other necessary dependencies here
+    ],
+)
