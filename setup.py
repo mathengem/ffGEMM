86e44834bf14b83205ecbbd53140d2680eb5dbfd
mathengem <mathengem@aol.com>
2025-02-18 01:44:44 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+   setup(
+       name='ffGEMM',
+       version='0.1.0',  # Replace with your desired version
+       packages=find_packages(),  # Automatically find packages within the repository
+       install_requires=[
+           # List any dependencies your package requires
+       ],
+   )
